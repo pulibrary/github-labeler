028d5eaf4148c4961447f3a2027fbc084ebf68a5
Sean Warren <seanwarren77@gmail.com>
2024-08-01 10:25:48 -0400

    update bin/labeler help
    Co-authored-by: Anna Headley <hackartisan@users.noreply.github.com>

--- lib/cli.rb
+++ lib/cli.rb
@@ -6,7 +6,7 @@ class LabelerCLI < Thor
     true
   end
 
-  desc "list_labels [org/repository]", "list all the labels in given repo"
+  desc "list_labels <org/repository>", "list all the labels in given repo"
   def list_labels(repo)
     Labeler.new.list_labels(repo).each do |pair|
       puts "#{pair[0]}, #{pair[1]}"
@@ -18,25 +18,25 @@ class LabelerCLI < Thor
     puts Labeler.new.categories
   end
 
-  desc "label_repo [org/repository] ", "apply all the labels to given repo"
+  desc "label_repo <org/repository> ", "apply all the labels to given repo"
   def label_repo(repo)
     Labeler.new.label_repo(repo)
   end
 
-  desc "label_repos --config=[config/dls.yml]", "apply all the labels to repos found in given config file"
-  option :config, required: true
+  desc "label_repos", "apply all the labels to repos found in given config file"
+  option :config, required: true, banner: "<relative_file_path>"
   def label_repos
     config_file = options[:config]
     Labeler.new.label_repos(config_file)
   end
 
-  desc "clear_labels [org/repository]", "delete all labels from the given repo"
+  desc "clear_labels <org/repository>", "delete all labels from the given repo"
   def clear_labels(repo)
     Labeler.new.clear_labels(repo)
   end
 
-  desc "delete_label --config=[config/dls.yml] label", "delete the label from the configured repos"
-  option :config, required: true
+  desc "delete_label <label>", "delete the label from the configured repos"
+  option :config, required: true, banner: "<relative_file_path>"
   def delete_label(label)
     config_file = options[:config]
     puts Labeler.new.delete_label(config_file, label)
